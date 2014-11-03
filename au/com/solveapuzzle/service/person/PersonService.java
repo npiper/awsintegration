52df32e6f6b4140b6f5a149336bd78c8a1da9d7e
Neil Piper <n_piper@hotmail.com>
2014-11-04 07:20:39 +1100

    Initial import of project structure. Builds model

---
+++ au/com/solveapuzzle/service/person/PersonService.java
@@ -0,0 +1,13 @@
+package au.com.solveapuzzle.service.person;
+
+/** Interface to represent the service facaded for a Person.
+ * Supports Query & Retrieval.
+ * 
+ * @author neilpiper
+ *
+ */
+public interface PersonService {
+
+	
+	
+}
