061bf2e8ef5b013ee76c9d9a08ac5510b62510aa
Neil Piper <n_piper@hotmail.com>
2015-01-07 00:16:48 +1100

    Removed generated files, added Impl file

---
+++ service/person/person.impl/src/main/java/au/com/solveapuzzle/service/person/PersonServiceImpl.java
@@ -0,0 +1,15 @@
+package au.com.solveapuzzle.service.person;
+
+import model.person.PersonRequestType;
+import model.person.PersonResponseType;
+
+public class PersonServiceImpl implements PersonService {
+
+	public PersonService internalFinder;
+	
+	public PersonResponseType findPerson(PersonRequestType request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
